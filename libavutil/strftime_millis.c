16a284380d5b38bf57660284ecb6c3031586938f
Alexey Obukhov <obukhoff@gmail.com>
2021-12-21 16:16:27 +0300

    fix-fix-fix

--- libavutil/strftime_millis.c
+++ libavutil/strftime_millis.c
@@ -4,18 +4,23 @@
 
 size_t strftime_millis(char* ptr, size_t maxsize,const char* format, const struct timeval* tv)
 {
-    struct tm *tm;
+    char* temp_name;
+    struct tm *tm, tmpbuf;
     size_t result;
-    char* temp_name = malloc(maxsize);
-    if ((tm = localtime(&(tv->tv_sec))) == NULL)
+    tm = localtime_r(&(tv->tv_sec), &tmpbuf);
+    if (tm == NULL)
     {
         return -1;
     }
 	
+    temp_name = malloc(maxsize);
     result = strftime(temp_name, maxsize, format, tm);
     if (result == 0)
+    {
+        free(temp_name);
         return -2;
-        
+    }
+
     result = snprintf(ptr, maxsize, temp_name, tv->tv_sec, tv->tv_usec);
     free(temp_name);
     return result;
